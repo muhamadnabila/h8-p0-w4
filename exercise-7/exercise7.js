2b16b994f0b0ef60fd974b03a50a9578a192542c
bilal <mbilalghz@gmail.com>
2018-12-14 15:07:28 +0700

    exercise-7

---
+++ exercise-7/exercise7.js
@@ -0,0 +1,14 @@
+function urutkanAbjad(str) {
+    var strSplit = str.split('')
+    var strSort = strSplit.sort()
+    var strJoin = strSort.join('')
+    
+    return strJoin
+  }
+  
+  // TEST CASES
+  console.log(urutkanAbjad('hello')); // 'ehllo'
+  console.log(urutkanAbjad('truncate')); // 'acenrttu'
+  console.log(urutkanAbjad('developer')); // 'deeeloprv'
+  console.log(urutkanAbjad('software')); // 'aeforstw'
+  console.log(urutkanAbjad('aegis')); // 'aegis'
